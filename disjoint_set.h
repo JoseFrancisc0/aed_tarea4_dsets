cd539cf42411c94a30869b63400585591b187e01
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-01-31 15:25:52 -0500

    Implemented main methods for DSetArray

--- disjoint_set.h
+++ disjoint_set.h
@@ -12,15 +12,46 @@ class Disjoint_Set{
 };
 
 class Disjoint_SetArray: public Disjoint_Set{
+    private:
+        int parent[100];
+        int rank[100];
     public:
-        Disjoint_SetArray();
+        Disjoint_SetArray(){
+            for(int i = 0; i < 100; i++){
+                parent[i] = 0;
+                rank[i] = 0;
+            }
+        }
 
-        void MakeSet(int x) override;
-        int Find(int x) override;
-        void Union(int x, int y) override;
+        void MakeSet(int x) override{
+            parent[x] = x;
+            rank[x] = 0;
+        }
+
+        int Find(int x) override{
+            if(parent[x] == x)
+                return x;
+            else
+                return Find(parent[x]);
+        }
+
+        void Union(int x, int y) override{
+            int xRoot = Find(x);
+            int yRoot = Find(y);
+
+            if(rank[xRoot] < rank[yRoot])
+                parent[xRoot] = yRoot;
+            else if(rank[xRoot] > rank[yRoot])
+                parent[yRoot] = xRoot;
+            else{
+                parent[yRoot] = xRoot;
+                rank[xRoot]++;
+            }
+        }
+
         bool isConected(int x, int y) override;
 
-        ~Disjoint_SetArray() override;
+        ~Disjoint_SetArray() override = default;
 };
 
 class Disjoint_SetTree: public Disjoint_Set{
