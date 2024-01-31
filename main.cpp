13be51e06d0d64d988fb3f2d539f32ee44a8e349
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-01-31 16:51:50 -0500

    Ya deberia estar, igual corrijan si algo esta mal

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
+#include "disjoint_set.h"
+using namespace std;
+
+void arrayTest(){
+    DisjointSetArray* set = new DisjointSetArray();
+
+    set->MakeSet(1);
+    set->MakeSet(2);
+    set->MakeSet(3);
+    set->MakeSet(4);
+    set->MakeSet(5);
+
+    set->Union(1,2);
+    set->Union(3,4);
+
+    cout << set->Find(1) << " : " << set->Find(2) << endl;
+    cout << set->Find(3) << " : " << set->Find(4) << endl;
+    cout << boolalpha << set->isConected(1,3) << endl;
+    
+    set->Union(1,3);
+    cout << boolalpha << set->isConected(1,3) << endl;
+
+    cout << set->Find(1) << " : " << set->Find(3) << endl;
+
+    set->Union(1,5);
+}
+
+void treeTest(){
+    DisjointSetTree* set = new DisjointSetTree();
+
+    set->MakeSet(1);
+    set->MakeSet(2);
+    set->MakeSet(3);
+    set->MakeSet(4);
+    set->MakeSet(5);
+
+    set->Union(1,2);
+    set->Union(3,4);
+
+    cout << set->Find(1) << " : " << set->Find(2) << endl;
+    cout << set->Find(3) << " : " << set->Find(4) << endl;
+    cout << boolalpha << set->isConected(1,3) << endl;
+    
+    set->Union(1,3);
+    cout << boolalpha << set->isConected(1,3) << endl;
+
+    cout << set->Find(1) << " : " << set->Find(3) << endl;
+
+    set->Union(1,5);
+}
 
 int main(int, char**){
-    std::cout << "Hello, from TAREA_SEMANA_4!\n";
+    //arrayTest();
+    //treeTest();
 }
